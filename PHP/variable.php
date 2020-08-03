df8428dcc629053c9efa9f37f7e76e58d53837cf
kang hyeon <kh90177287@gmail.com>
2020-08-04 00:32:40 +0900

    Create variable.php

---
+++ PHP/variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Variable</h1>
+    <?php 
+    $name = "egoing";
+    echo "Lorem ipsum dolor sit amet ".$name." consectetur adipisicing elit.
+    Laudantium, ".$name." necessitatibus quia reiciendis illum sint,
+    error quam laboriosam ".$name." tempore praesentium iste, similique nobis!
+    Explicabo, porro illum? ".$name." Fugiat accusantium soluta
+    dicta laudantium.";
+    ?>
+</body>
+
+</html>
