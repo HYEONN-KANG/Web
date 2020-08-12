ad3b2459082267a5ce4ce5a5ee49fed30621c873
kang hyeon <kh90177287@gmail.com>
2020-08-12 12:10:56 +0900

    study php&mysql

--- PHP&MySQL/create.php
+++ PHP&MySQL/create.php
@@ -4,28 +4,18 @@ $conn = mysqli_connect(
   'root',
   '111111',
   'opentutorials');
-
 $sql = "SELECT * FROM topic";
 $result = mysqli_query($conn, $sql);
 $list = '';
-while($row = mysqli_fetch_array($result)){
-    // <li><a href="index.php?id=19">MySQL</a></li>
-    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+while($row = mysqli_fetch_array($result)) {
+  $escaped_title = htmlspecialchars($row['title']);
+  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$escaped_title}</a></li>";
 }
-
 $article = array(
-    'title'=>'Welcome',
-    'description'=>'Hello, web'
+  'title'=>'Welcome',
+  'description'=>'Hello, web'
 );
-if(isset($_GET['id'])){
-$sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_array($result);
-$article['title'] = $row['title'];
-$article['description'] = $row['description'];
-}
 ?>
-
 <!doctype html>
 <html>
 
@@ -37,7 +27,7 @@ $article['description'] = $row['description'];
 <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
-        <?=$list;?>
+        <?=$list?>
     </ol>
     <form action="process_create.php" method="POST">
         <p><input type="text" name="title" placeholder="title"></p>
